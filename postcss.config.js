d979861d9240c9e327997f8e6311b775986ca227
robsavin <rob@bigtrailadventures.com>
2026-03-10 13:34:48 +0000

    Integrate rem-to-px plugin into PostCSS config
    Added a custom PostCSS plugin to convert rem units to px.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+const remToPx = () => ({
+  postcssPlugin: 'rem-to-px',
+  Declaration(decl) {
+    if (decl.value && decl.value.includes('rem')) {
+      decl.value = decl.value.replace(/(\d*\.?\d+)rem/g, (_, n) =>
+        `${Math.round(parseFloat(n) * 16)}px`
+      );
+    }
+  }
+});
+remToPx.postcss = true;
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    remToPx
+  ]
 };
