aa7111e825583bd4cb87be14b753412480842a07
robsavin <rob@bigtrailadventures.com>
2026-03-10 14:23:20 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "tailwindcss";
-
 export default {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
+  corePlugins: { preflight: false },
   theme: {
     container: {
       center: true,
