727e2c95c0337253c947aa368067eb94ee87d5ce
robsavin <rob@bigtrailadventures.com>
2026-03-10 13:36:55 +0000

    Refactor PostCSS configuration to use object syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
-const remToPx = () => ({
-  postcssPlugin: 'rem-to-px',
-  Declaration(decl) {
-    if (decl.value && decl.value.includes('rem')) {
-      decl.value = decl.value.replace(/(\d*\.?\d+)rem/g, (_, n) =>
-        `${Math.round(parseFloat(n) * 16)}px`
-      );
-    }
-  }
-});
-remToPx.postcss = true;
-
 export default {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-    remToPx
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
